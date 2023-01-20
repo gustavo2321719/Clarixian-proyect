a16f3a44244d3e70324ff1b884962d053a3a288d
gustavo2321719 <gustavo2321719@gmail.com>
2023-01-20 10:19:57 -0500

    script de conteo

---
+++ main.js
@@ -0,0 +1,5 @@
+let numero=1;
+while (numero>0 && numero<=217){
+    console.log('<img src="./cer/capitulo-34/cer_e32 ('+ numero +').jpg" alt="cer" class="imagen-proyecto">')
+    numero=numero+1
+}
