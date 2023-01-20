d53f3a04f954ec05f4178ccee10db026c9117ec9
gustavo2321719 <gustavo2321719@gmail.com>
2023-01-20 11:51:41 -0500

    z

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let numero=1;
 while (numero>0 && numero<=217){
-    console.log('<img src="./cer/capitulo-34/cer_e32 ('+ numero +').jpg" alt="cer" class="imagen-proyecto">')
+    console.log('<img src="./capitulo-34/cer_e32 ('+ numero +').jpg" alt="cer" class="capitulo">')
     numero=numero+1
 }
